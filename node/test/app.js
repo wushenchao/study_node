e25ee9aa713b6a87d71a8bf7315e8a09edc4e475
吴申超 <625574612@qq.com>
2015-08-19 13:53:16 +0800

    test

--- node/test/app.js
+++
@@ -1,3 +0,0 @@
-var circle = require ('./circle.js') ;
-console.log ('the area of a circle of radius 4 is '+ circle.area(4)) ;
-console.log ('the circumference of a circle of radius 4 is '+ circle.circumference(4)) ;
